2e65181bd09de4a98b01199ec03120ece9d9de01
m84013885 <84013885@qq.com>
2019-04-15 00:26:02 +0800

    fix : 增加方法

---
+++ gruntfile.js
@@ -0,0 +1,27 @@
+const name = 'line/';
+// const name = 'my/';
+// const name = 'paper/';
+
+const mozjpeg = require('imagemin-mozjpeg');
+
+module.exports = function (grunt) {
+    grunt.initConfig({
+        imagemin: {
+            dist: {
+                options: {
+                    optimizationLevel: 7, //定义 PNG 图片优化水平
+                    svgoPlugins: [{removeViewBox: false}],
+                    use: [mozjpeg()] // Example plugin usage
+                },
+                files: [{
+                    expand: true,
+                    cwd: name,   // 图片在imagemin目录下
+                    src: ['**/*.{png,jpg,jpeg}'], // 优化 imagemin 目录下所有 png/jpg/jpeg 图片
+                    dest: name // 优化后的图片保存位置，覆盖旧图片，并且不作提示
+                }]
+            }
+        },
+    });
+    grunt.loadNpmTasks('grunt-contrib-imagemin');
+    grunt.registerTask('default', ['imagemin']);
+};
